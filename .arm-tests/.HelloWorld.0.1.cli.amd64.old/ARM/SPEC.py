157956e1d52879561305b9d80e7ca44e7e31f0bf
Gabriel Margarido <lfilipe.ribeiro72@gmail.com>
2022-06-05 20:45:08 -0300

    Upgraded to ARM 2.0
    Fixed a bunch of bugs from version 1.0

---
+++ .arm-tests/.HelloWorld.0.1.cli.amd64.old/ARM/SPEC.py
@@ -0,0 +1,35 @@
+BINARY_PATH="/opt"
+BINARY_NAME="helloworld.bin"
+
+MAINTAINER="Gabriel Margarido"
+VERSION=0.1
+PACKAGE="Hello World CLI"
+
+OS_NAME="ArtemisOS"
+#OS_NAME="Linux"
+#OS_NAME="ArtemisBSD"
+#OS_NAME="FreeBSD"
+
+ARCH="x86_64"
+#ARCH="x86"
+#ARCH="amd64"
+#ARCH="i386"
+#ARCH="i486"
+#ARCH="i686"
+#ARCH="ia32"
+#ARCH="ppc"
+#ARCH="ppc64"
+#ARCH="ppc64-le"
+#ARCH="ppc-spe"
+#ARCH="riscv64"
+#ARCH="arm32"
+#ARCH="arm64"
+#ARCH="arm"
+#ARCH="aarch64"
+#ARCH="armv6"
+#ARCH="armv7"
+#ARCH="sparc64"
+#ARCH="aarch64"
+
+
+
---
+++ .arm-tests/HelloWorld.0.1.amd64/ARM/SPEC.py
@@ -0,0 +1,12 @@
+DESKTOP_SHORTCUT="/usr/share/applications/helloworld.desktop"
+
+BINARY_PATH="/opt/helloworld"
+BINARY_NAME="helloworld.bin"
+
+MAINTAINER="Gabriel Margarido"
+VERSION=0.1
+PACKAGE="TTS"
+
+ARCH="amd64"
+
+
